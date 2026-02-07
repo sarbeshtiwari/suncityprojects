a7ca68b8ff551d4fbae97b5091be408f2d0636bc
sarbesh <sarbeshtiwari39@gmail.com>
2026-02-07 10:52:51 +0530

    not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <html>
+      <body>
+        <h1>404 – Page Not Found</h1>
+        <p>The page you’re looking for doesn’t exist.</p>
+      </body>
+    </html>
+  );
+}
